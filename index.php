9c8258f898a66b075782b09de90f78f12819ebb4
kouceyla <hadjikouceyla@gmail.com>
2020-01-19 10:05:08 +0100

    Ajout du paragraphe

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <h1>Salam</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis cum reiciendis quas? Quod et fugit quia odio ipsa placeat ullam accusamus vitae commodi. Accusamus ad itaque neque quis cupiditate accusantium?</p>
