3977f528b0a3683b616155689aa12533058dce21
kouceyla <hadjikouceyla@gmail.com>
2020-01-19 10:13:46 +0100

    add header to index

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <h1>Salam</h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis cum reiciendis quas? Quod et fugit quia odio ipsa placeat ullam accusamus vitae commodi. Accusamus ad itaque neque quis cupiditate accusantium?</p>
+<header>
+    header
+</header>
