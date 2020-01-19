8f07bd1f2cc2f8a20659052be0cabca732822df1
kouceyla <hadjikouceyla@gmail.com>
2020-01-19 15:05:22 +0100

    Fix bug on index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1>Salam</h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis cum reiciendis quas? Quod et fugit quia odio ipsa placeat ullam accusamus vitae commodi. Accusamus ad itaque neque quis cupiditate accusantium?</p>
 <header>
-    header
+    en tete
 </header>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consequuntur provident libero est exercitationem quia corporis magnam recusandae minus, illo non quaerat quidem quod iusto dignissimos, nulla corrupti culpa aliquam!</p>
 <main>
