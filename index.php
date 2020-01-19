8da151356c9e4d5a1c577c283063ffe3246ca9ed
kouceyla <hadjikouceyla@gmail.com>
2020-01-19 14:18:00 +0100

    Add paragraph on index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <header>
     header
 </header>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consequuntur provident libero est exercitationem quia corporis magnam recusandae minus, illo non quaerat quidem quod iusto dignissimos, nulla corrupti culpa aliquam!</p>
 <main>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, deserunt accusantium perspiciatis quia consequuntur maxime vero enim! Quam deserunt officiis odio aspernatur debitis eius, culpa saepe sapiente. Aspernatur, eligendi distinctio.</p>
 </main>
