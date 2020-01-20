19f97194834a2010628c68ee6d14b84f5bc9de88
kouceyla <hadjikouceyla@gmail.com>
2020-01-20 12:17:33 +0100

    Add image fix #27

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h2>Pull Automatique</h2>
 <hr>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcWC8RqyBk8yLnkWw4iu_iIDLBIvhkiZWmj9E4OpUvrgN4iKEu" alt="">
 <h1>REBIAI Tarek </h1>
 <h1>Salam</h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis cum reiciendis quas? Quod et fugit quia odio ipsa placeat ullam accusamus vitae commodi. Accusamus ad itaque neque quis cupiditate accusantium?</p>
