7cc986a84b3f309c1f1cab94ee2253efad553d9f
kouceyla <hadjikouceyla@gmail.com>
2020-01-20 12:12:17 +0100

    test auto pull

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<h2>Pull Automatique</h2>
+<hr>
 <h1>REBIAI Tarek </h1>
 <h1>Salam</h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis cum reiciendis quas? Quod et fugit quia odio ipsa placeat ullam accusamus vitae commodi. Accusamus ad itaque neque quis cupiditate accusantium?</p>
